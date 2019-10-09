642b34631a7c05ab6d32da6ffdfa4cfdf078daba
Vasya Pupkin <vasya@localhost>
2019-10-09 20:06:57 +0300

    1 com

---
+++ login.php
@@ -0,0 +1,18 @@
+
+<div class="login">
+    <h1>Добро пожаловать в бюро переводов</h1>
+    <div class="form">
+        <form enctype="multipart/form-data" action="auth.php" method="POST">
+            <p><input class = "login_input" type="email" name = "email" placeholder="E-mail"></p>
+            <p><input class = "login_input" type="password" name = "password" placeholder="Password"></p>
+            <button>Отправить</button>
+        </form>
+    </div>
+
+<!--Админитсратор-->
+<!--email: mail@mail.ru-->
+<!--password": 12345-->
+
+<!--Переводчик-->
+<!--email: test@mail.ru-->
+<!--password:qwerty-->
